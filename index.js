cf5b51cfdac543660a679112201d1f35c74e0cc9
mehedi hasan <tanjildewan10@gmail.com>
2023-06-05 14:37:57 +0600

    menu

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 
 //middlewere
 app.use(cors())
 app.use(express.json())
+
+const verifyJWT = (req,res,next) =>{
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error:true,message:'unauthorize access'})
+  }
+
+  //bearer token
+  const token = authorization.split(' ')[1]
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+      if(err){
+        return res.status(401).send({error:true,message:'unauthorize access'})
+      }
+      req.decoded = decoded;
+      next()
+  })
+}
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -32,21 +50,72 @@ async function run() {
     const cartCollection = client.db("bistroDB").collection("cart");
 
 
+    app.post('/jwt',(req,res)=>{
+      const user = req.body;
+      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{ expiresIn: '1h' })
+      res.send({token})
+    })
+
+    const verifyAdmin = async(req,res,next)=>{
+        const email = req.decoded.email;
+        const query = {email:email}
+        const user = await usersCollection.findOne(query)
+        if(user?.role !== 'admin'){
+         return  res.status(403).send({error:true,message:'forbiden access'})
+        }
+        next()
+    }
+
+    /**
+     * 1.verify jwt token: varifyJWT
+     * 2.
+    */
+
+    app.get('/users',verifyJWT,verifyAdmin,async(req,res)=>{
+      const result = await usersCollection.find().toArray()
+      res.send(result)
+    })
 
     app.post('/users',async(req,res)=>{
       const user = req.body;
+      console.log(user)
       const query = {email:user.email}
-      const exsitingUser = usersCollection.findOne(query)
+      const exsitingUser = await usersCollection.findOne(query)
+      console.log(exsitingUser)
       if(exsitingUser){
         return res.send({message:'user already exsit'})
       }
       const result = await usersCollection.insertOne(user)
+      console.log(result)
       res.send(result)
     })
 
-    app.get('/users',async(req,res)=>{
-      const result = await usersCollection.find().toArray()
+    app.get('/users/admin/:email',verifyJWT,async(req,res)=>{
+      const email = req.params.email;
+
+      if(req.decoded.email !== email){
+        res.send({admin:false})
+      }
+
+      const query = {email:email}
+      const user = await usersCollection.findOne(query)
+      const result = {admin:user?.role === 'admin'}
+      console.log(result)
       res.send(result)
+    })
+
+    app.patch('/users/admin/:id',async(req,res)=>{
+         const id = req.params.id;
+         const filter = {_id: new ObjectId(id)}
+         const updateDoc = {
+          $set: {
+            role: 'admin'
+          },
+        };
+
+        const result = await usersCollection.updateOne(filter,updateDoc);
+        res.send(result)
+
     })
 
     app.get('/menu',async(req,res)=>{
@@ -54,16 +123,28 @@ async function run() {
         res.send(result)
     })
 
+    app.post('/menu',verifyJWT,verifyAdmin,async(req,res)=>{
+          const newItem = req.body;
+          const result = await menuCollection.insertOne(newItem)
+          res.send(result)
+    })
+
     app.get('/review',async(req,res)=>{
         const result = await reviewCollection.find().toArray()
         res.send(result)
     })
 
-    app.get('/carts',async(req,res)=>{
+    app.get('/carts',verifyJWT,async(req,res)=>{
       const email = req.query.email;
       if(!email){
         res.send([])
       }
+
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail){
+        return res.status(401).send({error:true,message:'porabiden access'})
+      }
+
       const query = { email: email };
       const result = await cartCollection.find(query).toArray()
       res.send(result)
